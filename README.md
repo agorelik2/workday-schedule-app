a86b7f95e880b207945afc0712f9a37f864cb071
Red Fairy <agorelik2@gmail.com>
2020-07-15 15:46:47 -0400

    streamlined js code, dynamically update time in seconds

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # workday-schedule-app
+# 05 Third-Party APIs: Work Day Scheduler
+
+Created a simple calendar application that allows the user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+## Purpose
+
+```
+The app allows to add important events to a daily planner
+to help employees manage their time effectively.
+```
+
+## Functionality
+
+```
+WHEN user opens the planner, the current day and time is displayed at the top of the calendar.
+WHEN user scrolls down, he/she is presented with timeblocks for standard business hours.
+WHEN user views the timeblocks for that day, each timeblock is color coded to indicate whether it is in the past, present, or future.
+WHEN user clicks into a timeblock, he/she can enter a new or update an existing event.
+To save the event, user has to click the save button for that timeblock.
+As the result, the text for that event is saved in local storage.
+WHEN user refreshes the page, the saved events persist.
+```
+
+The following animation demonstrates the application functionality:
+
+![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
+
+## Review
+
+You are required to submit the following for review:
+
+* The URL of the deployed application.
+
+* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,18 @@
-$(document).ready(function () {
-    //display current day & time.
-    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+var datetime = null,
+        date = null;
 
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function () {
+    //display current day & time
+    datetime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+
+    
     //Assign saveBtn click listener for user input and time stamp??
     $(".saveBtn").on("click", function () {
         
@@ -20,18 +31,9 @@ $(document).ready(function () {
     //Loop to load saved task descriptions from LocalStorage
     $(".description").each(function(){
         $(this).val(localStorage.getItem($(this).parent().attr("id")));
-        console.log("try this for local storage: " + localStorage.getItem($(this).parent().attr("id")))
+        
     })
-    //$("#hour9 .description").val(localStorage.getItem("hour9"));
-    //$("#hour10 .description").val(localStorage.getItem("hour10"));
-    //$("#hour11 .description").val(localStorage.getItem("hour11"));
-    //$("#hour12 .description").val(localStorage.getItem("hour12"));
-    // $("#hour13 .description").val(localStorage.getItem("hour13"));
-    //$("#hour14 .description").val(localStorage.getItem("hour14"));
-    //$("#hour15 .description").val(localStorage.getItem("hour15"));
-    //$("#hour16 .description").val(localStorage.getItem("hour16"));
-    //$("#hour17 .description").val(localStorage.getItem("hour17"));
-
+    
     //Loop over time blocks and color them based on past, present or future
     function taskTracker() {
         //Get current hour based on moment()
@@ -60,9 +62,9 @@ $(document).ready(function () {
             }
         })
     }
-    //taskTracker();
+    taskTracker();
 
-    //set up interval to check if current tasks have to be updated
+    //set up interval to check if current tasks have to be updated every 15 sec
     var interval = setInterval(taskTracker,15000);
 
 })
